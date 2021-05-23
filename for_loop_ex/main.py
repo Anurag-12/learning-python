3bba6fefad0bff8dbfabbf5bb4061faf3af38102
Anurag-12 <64366327+Anurag-12@users.noreply.github.com>
2021-05-23 20:35:33 +0530

    added while loop sample
    added while loop sample

--- for_loop_ex/main.py
+++ for_loop_ex/main.py
@@ -13,3 +13,17 @@ for items in list:
     if type(items) == int and items>=6:
 
         print(items)
+        
+        
+######
+#while loop sample 
+# Continuously takes input from user untill its greater than 100
+
+while(True):
+    inp = int(input("Enter a Number\n"))
+    if inp>100:
+        print("Congrats you have entered a number greater than 100\n")
+        break
+    else:
+        print("Try again!\n")
+        continue
