dd374faf5d63db07ba612cd60b4989295bc56818
Anurag-12 <64366327+Anurag-12@users.noreply.github.com>
2021-05-24 23:27:10 +0530

    function sample and doc string use

---
+++ Function_doc_string_sample/main.py
@@ -0,0 +1,20 @@
+# a = 9
+# b = 8
+# c = sum((a, b)) # built in function
+
+def function1(a, b):
+    print("Hello you are in function 1", a+b)
+
+def function2(a, b):
+    """This is a function which will calculate average of two numbers
+    this function doesnt work for three numbers"""
+    average = (a+b)/2
+    # print(average)
+    return average
+
+# v = function2(5, 7)
+# print(v)
+print(function2.__doc__)
+
+
+# Good practice to have doc string in fucntion. Doc string are always in the first line of a fucntion.
