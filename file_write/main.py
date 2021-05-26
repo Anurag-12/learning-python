9345d713fdc88cfd418e0ef77a95c2589f692ead
Anurag-12 <64366327+Anurag-12@users.noreply.github.com>
2021-05-26 22:51:40 +0530

    file write sample

---
+++ file_write/main.py
@@ -0,0 +1,15 @@
+f = open("anurag.txt", "w")
+a = f.write("Anurag bhai bahut achhe hain\n")
+print(a)   # count of number of character written
+f.close()
+
+# f = open("anurag.txt", "a")
+# a = f.write("Anurag bhai bahut achhe hain\n")
+# print(a)
+# f.close()
+
+
+# # Handle read and write both
+# f = open("anurag.txt", "r+")
+# print(f.read())
+# f.write("thank you")
